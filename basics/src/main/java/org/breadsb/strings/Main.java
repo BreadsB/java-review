fe5b2fbdbcff705c03225435cdc13c33a1cfa04a
BreadsB <mchlebek323@gmail.com>
2023-01-16 21:54:25 +0100

    learning access modifiers and strings

---
+++ basics/src/main/java/org/breadsb/strings/Main.java
@@ -0,0 +1,51 @@
+package org.breadsb.strings;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(greeting("Mike"));
+        System.out.println(greeting("Thomas"));
+        greeting2();
+        greeting3();
+        joinStrings("Beauti", "ful");
+        System.out.println(scanUserInput());
+    }
+
+    private static String greeting(String name) {
+        return String.format("Hello %s", name);
+    }
+
+    private static void greeting2() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("root");
+        System.out.println(sb);
+        sb.append("James");
+        System.out.println(sb);
+        sb.reverse();
+        System.out.println(sb);
+        sb.append("MonthlyRate");
+        System.out.println(sb.length());
+    }
+
+    private static void greeting3() {
+        String name = "Sam";
+        name = name.replace('S','R'); // Big letters matters!
+        System.out.println(name);
+    }
+
+    private static void joinStrings(String one, String two) {
+        one = one.concat(two);
+        System.out.println(one);
+    }
+
+    private static String scanUserInput() {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter first String");
+        String first = myScanner.nextLine();
+        System.out.println("Enter second String");
+        String second = myScanner.nextLine();
+
+        return first.concat(second);
+    }
+}
