e8f2aa6a8649b57916910df44313376f928fc15e
BreadsB <mchlebek323@gmail.com>
2023-01-28 21:27:30 +0100

    PhoneNumber excercise

--- basics/src/main/java/org/breadsb/strings/LogLevels.java
+++ basics/src/main/java/org/breadsb/strings/LogLevels.java
@@ -3,22 +3,10 @@ package org.breadsb.strings;
 public class LogLevels {
 
     public static void main(String[] args) {
-//        System.out.println(message("[ERROR]: Invalid operation"));
-//        System.out.println(message("[WARNING]:   Disk almost full\r\n"));
-//        System.out.println(logLevel("Invalid operation [ERROR]:"));
-//        System.out.println(logLevel("[ERROR]: Invalid operation"));
-//        System.out.println(reformat("[INFO]: Operation completed)"));
-//        System.out.println(reformat("[ERROR]: Invalid operation"));
         System.out.println(reformat("[WARNING]:   Disk almost full\r\n"));
-//        System.out.println(reformat("   Disk almost full\r\n [WARNING]:"));
     }
 
     static String message(String text) {
-/*        StringBuilder sb = new StringBuilder(text);
-        sb.delete(0, sb.indexOf(":")+1);
-        return sb.toString().strip();*/
-
-        // split string on two arrays at "]: " and pick second item of the array, then trim it.
         return text.split("]: ")[1]
                 .trim();
     }
@@ -26,9 +14,6 @@ public class LogLevels {
     static String logLevel(String text) {
         return text.substring(text.indexOf("[")+1, text.indexOf("]"))
                 .toLowerCase();
-//        return text.split("]: ")[0]
-//                .substring(1)
-//                .toLowerCase();
     }
 
     static String reformat(String text) {
---
+++ basics/src/main/java/org/breadsb/strings/PhoneNumber.java
@@ -0,0 +1,28 @@
+package org.breadsb.strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhoneNumber {
+
+    public String cleanNumber(String phoneNumber) {
+
+        List<String> numbers = new ArrayList<>(List.of(phoneNumber.trim().split("\\s|\\D|\\b1")));
+        String convertedNumber = makeStringFromArrayList(numbers);
+
+        System.out.println(convertedNumber);
+        return convertedNumber.toString();
+    }
+
+    private String makeStringFromArrayList(List<String> aL) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (String element : aL) {
+            if (!(element.equals(""))) {
+                sb.append(element);
+            }
+        }
+        return sb.toString();
+    }
+}
