e21b2f84d56a202a59c3537a9cf26c612c0226d0
BreadsB <102490720+BreadsB@users.noreply.github.com>
2023-02-25 10:20:25 +0100

    Comparing runtime of two methods in adding two strings excercise

--- basics/src/main/java/org/breadsb/strings/AddTwoStringNumbers.java
+++ basics/src/main/java/org/breadsb/strings/AddTwoStringNumbers.java
@@ -2,16 +2,16 @@ package org.breadsb.strings;
 
 // https://edabit.com/challenge/QwGgDuvWtFmDTeRHw
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AddTwoStringNumbers {
 
-
-
     public static String addStrNums(String num1, String num2) {
+
         long time1 = System.nanoTime();
-        int convertedNum1, convertedNum2;
+        BigInteger convertedNum1, convertedNum2;
         String result;
 
         if(num1.isBlank()) {
@@ -22,45 +22,46 @@ public class AddTwoStringNumbers {
             num2="0";
         }
 
-        if(num1.matches("\\d") && num2.matches("\\d")){
-            convertedNum1 = Integer.parseInt(num1);
-            convertedNum2 = Integer.parseInt(num2);
-            result = String.valueOf(convertedNum1+convertedNum2);
+        if(num1.matches("[0-9]+") && num2.matches("[0-9]+")){
+            convertedNum1 = new BigInteger(num1);
+            convertedNum2 = new BigInteger(num2);
+            result = String.valueOf(convertedNum1.add(convertedNum2));
         } else {
             result = "-1";
         }
 
         long time2 = System.nanoTime();
-        System.out.println(time2-time1);
+        System.out.println("Method with simple if: "+(time2-time1));
         return result;
     }
 
     public static String addStrNumsMethodTwo(String num1, String num2) {
-        long time1 = System.nanoTime();
 
+        long time1 = System.nanoTime();
         List<String> list = new ArrayList<String>(List.of(num1, num2));
-        List<Integer> listConverted = new ArrayList<>();
-        int sum;
+        List<BigInteger> listConverted = new ArrayList<>();
+        BigInteger sum;
+
         list.stream()
                 .forEach(i-> {
                     if (i.isBlank()) {
                         i="0";
                     }
-                    if (i.matches("\\d")) {
-                        listConverted.add(Integer.parseInt(i));
+                    if (i.matches("\\d+")) {
+                        listConverted.add(new BigInteger(i));
                     } else {
-                        listConverted.add(-1);
+                        listConverted.add(new BigInteger(String.valueOf(-1)));
                     }
                 });
 
-        if (listConverted.contains(-1)) {
-            sum = -1;
+        if (listConverted.contains(BigInteger.valueOf(-1))) {
+            sum = BigInteger.valueOf(-1);
         } else {
-            sum = listConverted.stream().reduce(0, Integer::sum);
+            sum = listConverted.stream().reduce(BigInteger.valueOf(0), BigInteger::add);
         }
 
         long time2 = System.nanoTime();
-        System.out.println(time2-time1);
+        System.out.println("Method with list and streams: " + (time2 - time1));
         return String.valueOf(sum);
     }
 }
--- basics/src/test/java/org/breadsb/strings/AddTwoStringNumbersTest.java
+++ basics/src/test/java/org/breadsb/strings/AddTwoStringNumbersTest.java
@@ -1,13 +1,16 @@
 package org.breadsb.strings;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.breadsb.strings.AddTwoStringNumbers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class AddTwoStringNumbersTest {
 
     @Test
+    @Order(1)
+    @DisplayName("Add two string numbers while numbers are Integers. Method 1.")
     public void testAddStrNumsWhileNumsAreIntegers() {
         //GIVEN
         String num1 = "5";
@@ -22,6 +25,8 @@ public class AddTwoStringNumbersTest {
     }
 
     @Test
+    @Order(2)
+    @DisplayName("Add two string numbers while one number have letters. Method 1.")
     public void testAddStrNumsWhileNumsAreLetters() {
         //GIVEN
         String num1 = "asdqwe";
@@ -36,6 +41,8 @@ public class AddTwoStringNumbersTest {
     }
 
     @Test
+    @Order(4)
+    @DisplayName("Add two string numbers while one number is blank. Method 1.")
     public void testAddStrNumsWhileNumIsEmpty() {
 
         //GIVEN
@@ -51,6 +58,8 @@ public class AddTwoStringNumbersTest {
     }
 
     @Test
+    @Order(5)
+    @DisplayName("Add two string numbers while one number is blank. Method 2.")
     void testAddStrNumsMethodTwoWhileNumsAreEmpty() {
 
         //GIVEN
@@ -66,6 +75,8 @@ public class AddTwoStringNumbersTest {
     }
 
     @Test
+    @Order(3)
+    @DisplayName("Add two string numbers while one number have letters. Method 2.")
     void testAddStrNumsMethodTwoWhileNumHaveLetters() {
         //GIVEN
         String num1 = "asdqwe";
@@ -76,6 +87,38 @@ public class AddTwoStringNumbersTest {
 
         //THEN
         String expected = "-1";
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("Add two string numbers while two numbers are very long. Method 1.")
+    void testAddLongStrNums() {
+        //GIVEN
+        String num1 = "1874682736267235927359283579235789257";
+        String num2 = "32652983572985729";
+
+        //WHEN
+        String result = addStrNums(num1, num2);
+
+        //THEN
+        String expected = "1874682736267235927391936562808774986";
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @Order(7)
+    @DisplayName("Add two string numbers while two numbers are very long. Method 2.")
+    void testAddLongStrNumsMethodTwo() {
+        //GIVEN
+        String num1 = "1874682736267235927359283579235789257";
+        String num2 = "32652983572985729";
+
+        //WHEN
+        String result = addStrNumsMethodTwo(num1, num2);
+
+        //THEN
+        String expected = "1874682736267235927391936562808774986";
         assertEquals(expected, result);
     }
 }
