7cf077a366760fc7c2cc694467c8c4740b377c13
BreadsB <102490720+BreadsB@users.noreply.github.com>
2023-02-25 23:11:41 +0100

    FindTheDifference excersice added

--- basics/src/main/java/org/breadsb/algorithms/Algorithms.java
+++ basics/src/main/java/org/breadsb/algorithms/Algorithms.java
@@ -1,13 +1,14 @@
 package org.breadsb.algorithms;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Algorithms {
 
     class FindTheDifference {
 //        https://edabit.com/challenge/JYaLkHy8cHgCPXAvy
         public static char findTheDifference(String s, String t) {
+            long time1 = System.nanoTime();
             List<String> one = new ArrayList<>(List.of(s.split("")));
             one.add("");
             List<String> two = new ArrayList<>(List.of(t.split("")));
@@ -18,6 +19,42 @@ public class Algorithms {
                     result = two.get(i).charAt(0);
                     break;
                 }
+            }
+            long time2 = System.nanoTime();
+            System.out.println(time2-time1);
+            return result;
+        }
+
+        public static char findTheDifference2(String s, String t) {
+            for (String a : s.split("")) {
+                t=t.replaceFirst(a,"");
+            }
+            return t.charAt(0);
+        }
+
+        public static char findTheDifference3(String s, String t) {
+            List<String> listOne = new ArrayList<>();
+            String[] splitedOne = s.split("");
+            for (String element : splitedOne) {
+                listOne.add(element);
+            }
+            List<String> listTwo = new ArrayList<>();
+            String[] splitedTwo = t.split("");
+            for (String element : splitedTwo) {
+                listTwo.add(element);
+            }
+            Collections.sort(listOne);
+            Collections.sort(listTwo);
+            char result = 0;
+
+            for (int i=0; i<listOne.size(); i++) {
+                if (!listTwo.get(i).equals(listOne.get(i))) {
+                    result = listTwo.get(i).charAt(0);
+                    break;
+                }
+            }
+            if (result==0) {
+                result = listTwo.get(listTwo.size()-1).charAt(0);
             }
             return result;
         }
