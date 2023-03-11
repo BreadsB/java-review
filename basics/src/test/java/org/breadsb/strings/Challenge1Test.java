73de869777ae54e8a7b21637b86a3f6a4eb7da86
BreadsB <102490720+BreadsB@users.noreply.github.com>
2023-03-11 14:07:13 +0100

    Check ending in String challenge added

---
+++ basics/src/test/java/org/breadsb/strings/Challenge1Test.java
@@ -0,0 +1,18 @@
+package org.breadsb.strings;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Challenge1Test {
+
+    @Test
+    void test1() {
+        Assertions.assertFalse(Challenge1.checkEnding("samurai", "zi"));
+        Assertions.assertTrue(Challenge1.checkEnding("feminine", "nine"));
+        Assertions.assertFalse(Challenge1.checkEnding("convention", "tio"));
+        Assertions.assertTrue(Challenge1.checkEnding("abc", "bc"));
+        Assertions.assertFalse(Challenge1.checkEnding("a", "romeo"));
+        Assertions.assertFalse(Challenge1.checkEnding("", "romeo"));
+        Assertions.assertTrue(Challenge1.checkEnding("   a ", "a "));
+    }
+}
