94420e90d39f9f3a6fe2969b623db8180344792e
BreadsB <102490720+BreadsB@users.noreply.github.com>
2023-05-06 13:33:24 +0200

    Added Mockito to IOSandboxTests

--- basics/src/test/java/org/breadsb/sandbox/IOSandboxTestSuite.java
+++ basics/src/test/java/org/breadsb/sandbox/IOSandboxTestSuite.java
@@ -2,18 +2,18 @@ package org.breadsb.sandbox;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
+import org.mockito.Mockito;
 
 public class IOSandboxTestSuite {
 
-    IOSandbox ioSandbox = new IOSandbox();
+    IOSandbox ioSandbox = Mockito.mock(IOSandbox.class);
 
     // How to virtually create a file? Use Mockito to only simulate a created file.
 
     @Test
     void testCreateNewFile() {
         String s = "CreatingNewTxtFileUsingJava";
+        Mockito.when(ioSandbox.createNewTextFile(s)).thenReturn(true);
         boolean created = ioSandbox.createNewTextFile(s);
         Assertions.assertTrue(created);
     }
