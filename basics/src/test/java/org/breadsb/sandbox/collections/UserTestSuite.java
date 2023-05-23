1f363ffc748b13a445547add6965935876488465
BreadsB <102490720+BreadsB@users.noreply.github.com>
2023-05-23 20:36:18 +0200

    Test User overridden hashCode() and equals() methods

--- basics/src/test/java/org/breadsb/sandbox/collections/UserTestSuite.java
+++ basics/src/test/java/org/breadsb/sandbox/collections/UserTestSuite.java
@@ -1,5 +1,19 @@
 package org.breadsb.sandbox.collections;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class UserTestSuite {
 
+    @Test
+    void testIfTwoUserObjectsAreEqual() {
+        User user1 = new User("Adam", "Eden", 27);
+        User user2 = new User("Ronald", "Grim", 30);
+        User user3 = new User("Adam", "Eden", 27, 400.0);
+        User user4 = new User("Ronald", "Grim", 30);
+
+        Assertions.assertNotEquals(user1, user2);
+        Assertions.assertNotEquals(user1, user3);
+        Assertions.assertEquals(user2, user4);
+    }
 }
