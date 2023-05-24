caf045412fc34b4310d08b5bd522d30c049fc300
BreadsB <102490720+BreadsB@users.noreply.github.com>
2023-05-24 19:17:35 +0200

    User hashCode test

--- basics/src/test/java/org/breadsb/sandbox/collections/UserTestSuite.java
+++ basics/src/test/java/org/breadsb/sandbox/collections/UserTestSuite.java
@@ -3,6 +3,10 @@ package org.breadsb.sandbox.collections;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class UserTestSuite {
 
     @Test
@@ -26,5 +30,23 @@ public class UserTestSuite {
         user.addRank(20.0);
         //THEN
         Assertions.assertEquals(20.0, user.getRank());
+    }
+
+    @Test
+    void putUsersToHashMapAndCheckEquality() {
+        //GIVEN
+        User user1 = new User("Adam", "Eden", 27);
+        User user2 = new User("Ronald", "Grim", 30);
+        User user3 = new User("Ronald", "Grim", 30);
+        Map<User, User> userMap = new HashMap<>();
+        //WHEN
+        userMap.put(user1, user1);
+        userMap.put(user2, user2);
+        userMap.put(user3, user3);
+        //THEN
+        Assertions.assertTrue(userMap.containsKey(user1));
+        Assertions.assertTrue(userMap.containsKey(user2));
+        Assertions.assertTrue(userMap.containsKey(user3));
+        Assertions.assertEquals(userMap.get(user2), userMap.get(user3));
     }
 }
