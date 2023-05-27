94b552a376232dc41d34558da76f9eba06f1e9f2
BreadsB <102490720+BreadsB@users.noreply.github.com>
2023-05-27 16:04:31 +0200

    Initialize Map using Java 9
    Using Map.of() and Map.ofEntries() to initialize new Map

--- basics/src/test/java/org/breadsb/sandbox/collections/MapsSandboxTestSuite.java
+++ basics/src/test/java/org/breadsb/sandbox/collections/MapsSandboxTestSuite.java
@@ -38,4 +38,29 @@ public class MapsSandboxTestSuite {
         Assertions.assertEquals(1, map.get("first"));
         Assertions.assertEquals(2, map.get("second"));
     }
+
+    @Test
+    void createMapUsingMapOfEntries() {
+        //GIVEN WHEN
+        Map<Integer, String> map = Map.ofEntries(
+                new AbstractMap.SimpleImmutableEntry<Integer, String>(1, "Mike"),
+                new AbstractMap.SimpleImmutableEntry<Integer, String>(2, "James"),
+                new AbstractMap.SimpleImmutableEntry<Integer, String>(3, "Ronald")
+        );
+
+        Assertions.assertEquals(3, map.size());
+        Assertions.assertEquals("James", map.get(2));
+    }
+
+    @Test
+    void createMapUsingMapOf() {
+        Map<Integer, String> emptyMap = Map.of();
+        Map<Integer, String> singletonMap = Map.of(10, "singleton value");
+        Map<Integer, String> multipleEntriesMap = Map.of(1, "value one", 2, "value two", 3, "value three");
+
+        Assertions.assertTrue(emptyMap.isEmpty());
+        Assertions.assertFalse(singletonMap.isEmpty());
+        Assertions.assertTrue(singletonMap.containsValue("singleton value"));
+        Assertions.assertEquals(3, multipleEntriesMap.size());
+    }
 }
