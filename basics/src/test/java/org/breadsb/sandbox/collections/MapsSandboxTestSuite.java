002c253f639191d4c13a97ab8a1f6c9ededf5a59
BreadsB <102490720+BreadsB@users.noreply.github.com>
2023-05-27 15:39:34 +0200

    initialize map using stream

--- basics/src/test/java/org/breadsb/sandbox/collections/MapsSandboxTestSuite.java
+++ basics/src/test/java/org/breadsb/sandbox/collections/MapsSandboxTestSuite.java
@@ -3,7 +3,10 @@ package org.breadsb.sandbox.collections;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.AbstractMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MapsSandboxTestSuite {
 
@@ -23,5 +26,16 @@ public class MapsSandboxTestSuite {
         Map<Integer, String> map = sandbox.createSingletonMap();
         //THEN
         Assertions.assertThrows(UnsupportedOperationException.class, ()-> map.put(5, "error"));
+    }
+
+    @Test
+    void createMapUsingStream() {
+        //GIVEN WHEN
+        Map<String, Integer> map = Stream.of(
+                new AbstractMap.SimpleEntry<>("first", 1),
+                new AbstractMap.SimpleEntry<>("second", 2)).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        //THEN
+        Assertions.assertEquals(1, map.get("first"));
+        Assertions.assertEquals(2, map.get("second"));
     }
 }
