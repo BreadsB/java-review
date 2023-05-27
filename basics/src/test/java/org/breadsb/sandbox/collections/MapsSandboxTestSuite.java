e597a6401f22dcae18220d75332aa715f6d79da5
BreadsB <102490720+BreadsB@users.noreply.github.com>
2023-05-27 15:23:17 +0200

    Test singleton map

--- basics/src/test/java/org/breadsb/sandbox/collections/MapsSandboxTestSuite.java
+++ basics/src/test/java/org/breadsb/sandbox/collections/MapsSandboxTestSuite.java
@@ -1,5 +1,27 @@
 package org.breadsb.sandbox.collections;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
 public class MapsSandboxTestSuite {
 
+    MapSandbox sandbox = new MapSandbox();
+
+    @Test
+    void createSingletonImmutableMap() {
+        //GIVEN WHEN
+        Map<Integer, String> map = sandbox.createSingletonMap();
+        //THEN
+        Assertions.assertEquals("Config", map.get(0));
+    }
+
+    @Test
+    void modifySingletonMap() {
+        //GIVEN WHEN
+        Map<Integer, String> map = sandbox.createSingletonMap();
+        //THEN
+        Assertions.assertThrows(UnsupportedOperationException.class, ()-> map.put(5, "error"));
+    }
 }
