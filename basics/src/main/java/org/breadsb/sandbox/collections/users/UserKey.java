c745fb76e4a1b8707b004bfa94c13358e3a4a053
BreadsB <102490720+BreadsB@users.noreply.github.com>
2023-05-27 12:10:36 +0200

    hashCode and equals added

--- basics/src/main/java/org/breadsb/sandbox/collections/users/UserKey.java
+++ basics/src/main/java/org/breadsb/sandbox/collections/users/UserKey.java
@@ -1,5 +1,7 @@
 package org.breadsb.sandbox.collections.users;
 
+import java.util.Arrays;
+
 public final class UserKey {
 
     private final byte[] keyArray;
@@ -10,5 +12,18 @@ public final class UserKey {
 
     public byte[] getKeyArray() {
         return keyArray.clone();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserKey userKey = (UserKey) o;
+        return Arrays.equals(keyArray, userKey.keyArray);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(keyArray);
     }
 }
